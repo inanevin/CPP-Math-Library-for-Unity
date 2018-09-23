0056b7ea9814be7708bf35e43dd92126e9d78db6
Inan Evin <inanevin@gmail.com>
2018-09-24 02:22:36 +0300

    C Linkage Tests - IUnityInterface implementation

--- C++MathLibUnity/CalculationLibrary.cpp
+++ C++MathLibUnity/CalculationLibrary.cpp
@@ -1,16 +1,53 @@
 #include "stdafx.h"
 #include <stdlib.h>     /* srand, rand */
+#include <math.h>
+#include <IUnityInterface.h>
+#include <Windows.h>
 #include "CalculationLibrary.h"
+
+#define LIBFUNC_API(rtype) UNITY_INTERFACE_EXPORT rtype UNITY_INTERFACE_API
+
 
 #pragma region Utility
 
-float RandomizeFloats(float min, float max)
+/*double FindDistanceBetweenVector3s(V3* p1, V3* p2)
 {
-	float random = ((float)rand()) / (float)max;
-	float diff = max - min;
-	float r = random * diff;
+	// Store the differences.
+	double xDiff = p1->x - p2->x;
+	double yDiff = p1->y - p2->y;
+	double zDiff = p1->z - p2->z;
+
+	// Store the powers of the differences to the 2.
+	double powX = pow(xDiff, 2);
+	double powY = pow(yDiff, 2);
+	double powZ = pow(zDiff, 2);
+
+	// Apply the distance formula.
+	return sqrt(powX + powY + powZ);
+}*/
+
+double FindDistanceBetweenVector2s(V3* p1, V3* p2)
+{
+	// Store the differences.
+	double xDiff = p1->x - p2->x;
+	double yDiff = p1->y - p2->y;
+
+	// Store the powers of the differences to the 2.
+	double powX = pow(xDiff, 2);
+	double powY = pow(yDiff, 2);
+
+	// Apply the distance formula.
+	return sqrt(powX + powY);
+}
+
+double Randomizedoubles(double min, double max)
+{
+	double random = ((double)rand()) / (double)max;
+	double diff = max - min;
+	double r = random * diff;
 	return min + r;
 }
+
 int RandomizeInts(int min, int max)
 {
 	return rand() % min + max;
@@ -25,54 +62,66 @@ extern "C"
 
 #pragma region VectorRandomization
 
-	// Vector3 randomization methods & overloads for float and integer, as well as per property randomization & all around randomization.
-	void RandomizeVector3(V3* v, float min, float max)
+	/* Randomization functions for vectors. */
+
+	// Vector3 randomization methods & overloads for double and integer, as well as per property randomization & all around randomization.
+/*	void RandomizeVector3double(V3* v, double min, double max)
 	{
-		v->x = RandomizeFloats(min, max);
-		v->y = RandomizeFloats(min, max);
-		v->z = RandomizeFloats(min, max);
+		v->x = Randomizedoubles(min, max);
+		v->y = Randomizedoubles(min, max);
+		v->z = Randomizedoubles(min, max);
 	}
-	void RandomizeVector3(V3* v, float minX, float maxX, float minY, float maxY, float minZ, float maxZ)
+	void RandomizeVector3doubleRange(V3* v, double minX, double maxX, double minY, double maxY, double minZ, double maxZ)
 	{
-		v->x = RandomizeFloats(minX, maxX);
-		v->y = RandomizeFloats(minY, maxY);
-		v->z = RandomizeFloats(minZ, maxZ);
+		v->x = Randomizedoubles(minX, maxX);
+		v->y = Randomizedoubles(minY, maxY);
+		v->z = Randomizedoubles(minZ, maxZ);
 	}
-	void RandomizeVector3(V3* v, int min, int max)
+	void RandomizeVector3Int(V3* v, int min, int max)
 	{
 		v->x = RandomizeInts(min, max);
 		v->y = RandomizeInts(min, max);
 		v->z = RandomizeInts(min, max);
 	}
-	void RandomizeVector3(V3* v, int minX, int maxX, int minY, int maxY, int minZ, int maxZ)
+	void RandomizeVector3IntRange(V3* v, int minX, int maxX, int minY, int maxY, int minZ, int maxZ)
 	{
 		v->x = RandomizeInts(minX, maxX);
 		v->y = RandomizeInts(minY, maxY);
 		v->z = RandomizeInts(minZ, maxZ);
 	}
 
-	// Vector2 randomization methods & overloads for float and integer, as well as per property randomization & all around randomization.
-	void RandomizeVector2(V2* v, float min, float max)
+	// Vector2 randomization methods & overloads for double and integer, as well as per property randomization & all around randomization.
+	void RandomizeVector2double(V2* v, double min, double max)
 	{
-		v->x = RandomizeFloats(min, max);
-		v->y = RandomizeFloats(min, max);
+		v->x = Randomizedoubles(min, max);
+		v->y = Randomizedoubles(min, max);
 	}
-	void RandomizeVector2(V2* v, float minX, float maxX, float minY, float maxY)
+	void RandomizeVector2doubleRange(V2* v, double minX, double maxX, double minY, double maxY)
 	{
-		v->x = RandomizeFloats(minX, maxX);
-		v->y = RandomizeFloats(minY, maxY);
+		v->x = Randomizedoubles(minX, maxX);
+		v->y = Randomizedoubles(minY, maxY);
 	}
-	void RandomizeVector2(V2* v, int min, int max)
+	void RandomizeVector2Int(V2* v, int min, int max)
 	{
 		v->x = RandomizeInts(min, max);
 		v->y = RandomizeInts(min, max);
 	}
-	void RandomizeVector2(V2* v, int minX, int maxX, int minY, int maxY)
+	void RandomizeVector2IntRange(V2* v, int minX, int maxX, int minY, int maxY)
 	{
 		v->x = RandomizeInts(minX, maxX);
 		v->y = RandomizeInts(minY, maxY);
 	}
-
+	*/
 #pragma endregion
+
+	LIBFUNC_API(V3) FindClosest(V3* first, int size)
+	{
+		
+		V3* closestPoint = (V3*)malloc(sizeof(V3*));
+		closestPoint->x = 12;
+		//closestPoint = first + size;
+		return *(closestPoint);
+		
+	}
 
 }
--- C++MathLibUnity/CalculationLibrary.h
+++ C++MathLibUnity/CalculationLibrary.h
@@ -1,27 +1,34 @@
 #pragma once
 
-#define LIBFUNC_API __declspec (dllexport)
+#include <IUnityInterface.h>
 
-float RandomizeFloats(float,float);
+#define LIBFUNC_API(rtype) UNITY_INTERFACE_EXPORT rtype UNITY_INTERFACE_API
+//double FindDistanceBetweenVector3s(V3*, V3*);
+
+double Randomizedoubles(double, double);
 int RandomizeInts(int, int);
 struct V2 { float x, y; };
-struct V3 { float x, y, z; };
+struct V3 { double x, y, z; };
 
 extern "C"
 {
 
 #pragma region VectorRandomization
 
-	LIBFUNC_API void RandomizeVector3(V3* v, float, float);
-	LIBFUNC_API void RandomizeVector3(V3* v, float, float, float, float, float, float);
+	/*LIBFUNC_API void RandomizeVector3double(V3*, double, double);
+	LIBFUNC_API void RandomizeVector3doubleRange(V3* v, double, double, double, double, double, double);
 
-	LIBFUNC_API void RandomizeVector3(V3* v, int, int);
-	LIBFUNC_API void RandomizeVector3(V3* v, int, int, int, int, int, int);
+	LIBFUNC_API void RandomizeVector3Int(V3*, int, int);
+	LIBFUNC_API void RandomizeVector3IntRange(V3*, int, int, int, int, int, int);
 
-	LIBFUNC_API void RandomizeVector2(V2* v, float, float);
-	LIBFUNC_API void RandomizeVector2(V2* v, float, float, float, float);								   
-	LIBFUNC_API void RandomizeVector2(V2* v, int, int);
-	LIBFUNC_API void RandomizeVector2(V2* v, int, int, int, int);
+	LIBFUNC_API void RandomizeVector2double(V2*, double, double);
+	LIBFUNC_API void RandomizeVector2doubleRange(V2*, double, double, double, double);								   
+	LIBFUNC_API void RandomizeVector2Int(V2*, int, int);
+	LIBFUNC_API void RandomizeVector2IntRange(V2*, int, int, int, int);
+
+	LIBFUNC_API V3* FindClosest(V3*, V3*, int);*/
+
+	//LIBFUNC_API(V3 *) FindClosest(int);
 
 #pragma endregion
 
